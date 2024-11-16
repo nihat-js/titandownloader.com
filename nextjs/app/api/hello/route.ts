333371315118b36302f59bb56540cf891ec0f10a
Nihat <abdullazadenihat@gmail.com>
2024-11-17 03:24:55 +0400

    fixed bugs

--- nextjs/app/api/hello/route.ts
+++ nextjs/app/api/hello/route.ts
@@ -1,14 +1,14 @@
 import Pinterest from "../../services/Pinterest"
 export async function GET() {
 
-  let result
+  // let result
 
-  let result = Pinterest.fetch('https://www.pinterest.com/pin/839569555518499/')
+  // let result = Pinterest.fetch('https://www.pinterest.com/pin/839569555518499/')
 
 
   return new Response(JSON.stringify({
     message: 'Hello World!', 
-    url: result.url
+    // url: result.url
   }), {
     status: 200,
     headers: {
