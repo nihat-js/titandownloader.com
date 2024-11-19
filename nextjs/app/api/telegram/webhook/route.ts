c92bd5d936b69b6d1825391bac49146e66a0ace0
Nihat <abdullazadenihat@gmail.com>
2024-11-20 01:41:45 +0400

    fixed bugs

--- nextjs/app/api/telegram/webhook/route.ts
+++ nextjs/app/api/telegram/webhook/route.ts
@@ -5,6 +5,7 @@ import { Downloader } from '../../../services/Downloader';
 import { NextRequest, NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 import { downloadFromPinterest } from '@/app/services/Pinterest';
+import { downloadFromInstagram } from '@/app/services/Instagram';
 
 
 // update_id: 833496806,
@@ -15,6 +16,16 @@ import { downloadFromPinterest } from '@/app/services/Pinterest';
 //       date: 1731867180,
 //       text: 'b'
 //     }
+
+// {
+//   from: {
+//     id: 956543322,
+//     is_bot: false,
+//     first_name: 'Nihat',
+//     username: 'nih1t',
+//     language_code: 'en'
+//   }
+// }
 
 const testURLS = [
   "https://www.tiktok.com/@mavi.iva/video/7437591845573496072?is_from_webapp=1",
@@ -27,15 +38,14 @@ export async function POST(req: NextRequest,) {
 
   const url = new URL(req.url as string).searchParams.get('url');
   const data = await req.json();
-
-
   const { text, from, chat } = data.message
-  console.log(`Received message from ${from.first_name} (@${from.username}) ${from.user_id} : ${text}`);
+
+  console.log({from})
+  console.log(`Received message from ${from.first_name} (@${from.username}) ${from.id} : ${text}`);
   let replyText = `Hi ${from.first_name}, you said: ${text}`;
   const coins = 5
-  await sendVideoToTelegram(chat.id,"https://media.sssinstagram.com/get?__sig=f7-rFOWRFmEqLq64XST69Q&__expires=1732052109&uri=https%3A%2F%2Finstagram.fsyd14-1.fna.fbcdn.net%2Fo1%2Fv%2Ft16%2Ff1%2Fm86%2F5E4D4234E38E19549FC72F8BC2B041AC_video_dashinit.mp4%3Fstp%3Ddst-mp4%26efg%3DeyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0%26_nc_cat%3D101%26vs%3D1310419950369087_743177109%26_nc_vs%3DHBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC81RTRENDIzNEUzOEUxOTU0OUZDNzJGOEJDMkIwNDFBQ192aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dBR0lyaHZFT3NxTTRjZ0RBTnR0QUU5ODV0QnVicV9FQUFBRhUCAsgBACgAGAAbABUAACaGr4Cy8unNPxUCKAJDMywXQDEAAAAAAAAYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%253D%253D%26_nc_rid%3D84b63a947a%26ccb%3D9-4%26oh%3D00_AYCK6nBRH4sh91Fm5YI6IXQrZtJ_h_zH3LFvTrPD73lBlA%26oe%3D673E3995%26_nc_sid%3Dd885a2%26dl%3D1&filename=Fikirl%C9%99riniz.mp4&ua=-&referer=https%3A%2F%2Fwww.instagram.com%2F")
-
-  return ;
+  // await sendVideoToTelegram(chat.id,"https://media.sssinstagram.com/get?__sig=f7-rFOWRFmEqLq64XST69Q&__expires=1732052109&uri=https%3A%2F%2Finstagram.fsyd14-1.fna.fbcdn.net%2Fo1%2Fv%2Ft16%2Ff1%2Fm86%2F5E4D4234E38E19549FC72F8BC2B041AC_video_dashinit.mp4%3Fstp%3Ddst-mp4%26efg%3DeyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0%26_nc_cat%3D101%26vs%3D1310419950369087_743177109%26_nc_vs%3DHBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC81RTRENDIzNEUzOEUxOTU0OUZDNzJGOEJDMkIwNDFBQ192aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dBR0lyaHZFT3NxTTRjZ0RBTnR0QUU5ODV0QnVicV9FQUFBRhUCAsgBACgAGAAbABUAACaGr4Cy8unNPxUCKAJDMywXQDEAAAAAAAAYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%253D%253D%26_nc_rid%3D84b63a947a%26ccb%3D9-4%26oh%3D00_AYCK6nBRH4sh91Fm5YI6IXQrZtJ_h_zH3LFvTrPD73lBlA%26oe%3D673E3995%26_nc_sid%3Dd885a2%26dl%3D1&filename=Fikirl%C9%99riniz.mp4&ua=-&referer=https%3A%2F%2Fwww.instagram.com%2F")
+  // return ;
 
   if (text.toLowerCase() === '/start') {
     replyText = 'Hi you can send me Pinterest,Tiktok links and I will download the video for you!';
@@ -47,22 +57,25 @@ export async function POST(req: NextRequest,) {
   }
 
 
-  let downloader = new Downloader(text)
-  let result = await downloader.download()
+  // let downloader = new Downloader(text)
+  // console.log({text})
+  // return;
+  let result = await downloadFromInstagram(text)
+  
 
   // let source = await downloader.getSource()
   // let source = await downloadFromPinterest(text)
-  if (result.length == 0){
+  if (!result || result.length == 0) {
     await sendMessage(chat.id, 'Sorry, I could not download. Please try again later.')
     return new Response("Ok")
   }
 
-  // @ts-ignore
-  result.forEach(async (element) =>{
+  // @ts-expect-error Bilmirem niye error verir
+  result.forEach(async (element) => {
     sendVideoToTelegram(chat.id, element.url)
   })
-  
-    return new Response("Retrieved")
+
+  return new Response("Retrieved")
 }
 
 
--- nextjs/app/services/Instagram.ts
+++ nextjs/app/services/Instagram.ts
@@ -10,47 +10,57 @@ export async function downloadFromInstagram(url: string) {
 
   const browser = await chromium.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto("https://sssinstagram.com", {  waitUntil: 'networkidle' });
+  await page.goto("https://en1.savefrom.net/2ol/", {  waitUntil: 'networkidle' });
+  await page.type('#sf_url', url);
+  await page.click('#sf_submit');
+  await page.waitForSelector('a[download]'); // Ensure <a> elements with the download attribute are loaded
+  const mediaLinks = await page.evaluate(() => {
+    const links = Array.from(document.querySelectorAll('a[download]')); // Select all <a> with download attribute
+    return links.map(link => {
+      const url = link.getAttribute('data-type') || ""
+      const mediaType = url.endsWith('.mp4') ? 'video' : 'unknown'; // Check if the URL ends with .mp4 for video type
+      return { url, mediaType };
+    });
+  });
 
-  const content = await page.content();
-  const $ = cheerio.load(content);
+  return mediaLinks;
 
   // Extract data using cheerio if needed
   // Example: const data = $('selector').text();
 
-  await browser.close();
+  // await browser.close();
   
 
-  let response = await fetch("https://sssinstagram.com/api/convert", {
-    "headers": {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "en-US,en;q=0.9",
-      "content-type": "application/json",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "cookie": "uid=17c8241ab3bbb999; clickAds=86"
-    },
-    "referrerPolicy": "no-referrer",
-    "body": JSON.stringify({ url,  _s: "e1ac422e98ca921340a43cd924cf6b7b592e5c1c0795630853bc2e57f4d951e9" }),
-    "method": "POST"
-  })
-  console.log("29a geldim")
-  let data = await response.json()
-  let newData: any = []
+  // let response = await fetch("https://sssinstagram.com/api/convert", {
+  //   "headers": {
+  //     "accept": "application/json, text/plain, */*",
+  //     "accept-language": "en-US,en;q=0.9",
+  //     "content-type": "application/json",
+  //     "priority": "u=1, i",
+  //     "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+  //     "sec-ch-ua-mobile": "?0",
+  //     "sec-ch-ua-platform": "\"Windows\"",
+  //     "sec-fetch-dest": "empty",
+  //     "sec-fetch-mode": "cors",
+  //     "sec-fetch-site": "same-origin",
+  //     "cookie": "uid=17c8241ab3bbb999; clickAds=86"
+  //   },
+  //   "referrerPolicy": "no-referrer",
+  //   "body": JSON.stringify({ url,  _s: "e1ac422e98ca921340a43cd924cf6b7b592e5c1c0795630853bc2e57f4d951e9" }),
+  //   "method": "POST"
+  // })
+  // console.log("29a geldim")
+  // let data = await response.json()
+  // let newData: any = []
 
 
   // @ts-ignore
-  newData.forEach(element => {
-    newData.push({
-      type: element.type,
-      url: element.url[0].url
-    })
-  });
+  // newData.forEach(element => {
+  //   newData.push({
+  //     type: element.type,
+  //     url: element.url[0].url
+  //   })
+  // });
   console.log(data)
   console.log(newData)
   return newData
